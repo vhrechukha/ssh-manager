0469283b77682e3fb93e02b7e85384e7ccb73928
Viktoriia Hrechukha <viktoriia.hrechukha@gmail.com>
2023-04-08 22:08:29 +0200

    [ba]: removed no needed dependencies

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,11 @@ mod cli;
 mod domain;
 mod repositories;
 
-use clap::{App, Arg};
 use repositories::config::InMemoryRepository;
 use std::sync::Arc;
 
 fn main() {
     let repo = Arc::new(InMemoryRepository::new());
 
-    let matches = App::new("Ssh-Manager")
-        .arg(Arg::with_name("cli").long("cli").help("Runs in CLI mode"))
-        .get_matches();
-
-    match matches.occurrences_of("cli") {
-        _ => cli::run(repo),
-    }
+    cli::run(repo);
 }
